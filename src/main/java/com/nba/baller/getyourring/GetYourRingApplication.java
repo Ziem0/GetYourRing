17ecb7a64beb0386b21d8c322c1bd63ef1d7fcef
Ziem0 <andrzejewski.ziemo@gmail.com>
2018-11-23 20:20:59 +0100

    init commit

---
+++ src/main/java/com/nba/baller/getyourring/GetYourRingApplication.java
@@ -0,0 +1,12 @@
+package com.nba.baller.getyourring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetYourRingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetYourRingApplication.class, args);
+	}
+}
